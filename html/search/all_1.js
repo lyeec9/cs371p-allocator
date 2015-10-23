5a4663208cfe525e3778535db340147b1ffd7b1c
Naeem Quddus <nquddus@cs.utexas.edu>
2015-10-22 20:19:46 -0500

    Added code documentation and ran Doxygen to create the html/ folder. This fixes #11. This fixes #12.

---
+++ html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['construct',['construct',['../classAllocator.html#a6e6a26ece248be1eb76ed691c085cd65',1,'Allocator']]],
+  ['cs371p_2dallocate',['CS371p-allocate',['../md_README.html',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cs371p_2dallocate',['CS371p-allocate',['../md_README.html',1,'']]]
+];
